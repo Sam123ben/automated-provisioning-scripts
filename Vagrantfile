aa82cf04f95aa43b98006deeb5626c80dd8a60f3
Samyak Rout <rout.samyak@nab.com.au>
2018-03-02 01:12:52 +1100

    Testing the Vagrant to auto configure port and 443 for ssl encrypted jenkins

--- Vagrantfile
+++ Vagrantfile
@@ -5,16 +5,24 @@ Vagrant.configure("2") do |config|
     sudo yum update -y && yum install -y python ansible git \
          && yum install -y yum-utils device-mapper-persistent-data lvm2 \
          && yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo \
-         && yum-config-manager --enable docker-ce-edge && yum install -y docker-ce epel-release nginx \
+         && systemctl enable firewalld && systemctl start firewalld && systemctl status firewalld \
+         && yum-config-manager --enable docker-ce-edge && yum install -y docker-ce epel-release nginx mod_ssl FirewallD \
          && usermod -aG docker vagrant \
          && systemctl enable docker && systemctl start docker && systemctl status docker \
-         && systemctl enable nginx && systemctl start nginx && systemctl start nginx \
+         && systemctl enable nginx && systemctl start nginx && systemctl status nginx \
          && docker run hello-world \
-         && docker ps -a
+         && docker ps -a \
+         && mkdir /etc/ssl/private && mkdir /etc/nginx/sites-enabled \
+         && chmod 700 /etc/ssl/private \
+         && firewall-cmd --add-service=http && firewall-cmd --add-service=https && firewall-cmd --runtime-to-permanent \
+         && iptables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT && iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+
+
 
     git clone -b feature/vagrant-setup https://github.com/Sam123ben/vibrato-code-test.git
-    cd 
-    sudo sh /home/vagrant/vibrato-code-test/docker-repo/jenkins/vagrant-auto-build.sh 'qwertyuiop' 'jenkins-latest-master-image'
+    sudo ansible-playbook /home/vagrant/vibrato-code-test/docker-repo/jenkins/Utilities/ansible-playbooks/samyak.configure-nginx.yml
+    sudo systemctl restart nginx && systemctl status nginx
+    sudo sh /home/vagrant/vibrato-code-test/docker-repo/jenkins/vagrant-auto-build.sh
   SHELL
 
 end
--- docker-repo/jenkins/vagrant-auto-build.sh
+++ docker-repo/jenkins/vagrant-auto-build.sh
@@ -7,31 +7,31 @@ master_redirect_port="8080"
 agent_redirect_port='8085'
 
 echo '\nThe known vault password: '
-$1
+vault_pass="qwertyuiop"
 
 echo '\n\nThe unique Image Name to be created: '
-$2
+image_name="jenkins-latest"
 
 echo '\n\nThe version of Jenkins you wish to install'
-$3
+jenkins_version=""
 
-if [ -n "$3" ]
+if [ -n "$jenkins_version" ]
 then
-      echo "\n\nThe jenkins version that will be installed is: $3"
+      echo "\n\nThe jenkins version that will be installed is: $jenkins_version"
       echo '\n\nBuilding the Docker-jenkins image\n'
-      jenkins_version_run_cmd="--extra-vars "JENKINS_VERSION=$3""
-      docker image build -t $2 --build-arg master_port=$master_port --build-arg agent_port=$agent_port --build-arg VAULT_PWD=$1 --build-arg jenkins_version=$3  --build-arg jenkins_master_run_cmd="$jenkins_version_run_cmd" .
+      jenkins_version_run_cmd="--extra-vars "JENKINS_VERSION=$jenkins_version""
+      docker image build -t $image_name --build-arg master_port=$master_port --build-arg agent_port=$agent_port --build-arg VAULT_PWD=$vault_pass --build-arg jenkins_version=$jenkins_version  --build-arg jenkins_master_run_cmd="$jenkins_version_run_cmd" .
 else
       echo "\nThe jenkins version that will be installed is: latest"
       echo '\nBuilding the latest version of Docker-jenkins image\n'
-      docker image build -t $2 --build-arg master_port=$master_port --build-arg agent_port=$agent_port --build-arg VAULT_PWD=$1 .
+      docker image build -t $image_name --build-arg master_port=$master_port --build-arg agent_port=$agent_port --build-arg VAULT_PWD=$vault_pass .
 fi
 
-container_name=$2-app
+container_name=$image_name-app
 sleep 10s
 
 echo '\nRunning the container'
-docker run --restart=always --name $container_name -u jenkins -p $master_redirect_port:$master_port -p $agent_redirect_port:$agent_port -d $2
+docker run --restart=always --name $container_name -u jenkins -p $master_redirect_port:$master_port -p $agent_redirect_port:$agent_port -d $image_name
 
 sleep 5s
 docker ps
