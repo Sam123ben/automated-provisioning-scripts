0d7e295655c0d3fa97e8d921007fa00e904ba5f6
Samyak Rout <rout.samyak@nab.com.au>
2018-03-02 03:35:07 +1100

    test successfully on Vagrant to run Jenkins docker image

--- docker-repo/jenkins/vagrant-auto-build.sh
+++ docker-repo/jenkins/vagrant-auto-build.sh
@@ -12,20 +12,9 @@ vault_pass="qwertyuiop"
 echo '\n\nThe unique Image Name to be created: '
 image_name="jenkins-latest"
 
-echo '\n\nThe version of Jenkins you wish to install'
-jenkins_version=""
-
-if [ -n "$jenkins_version" ]
-then
-      echo "\n\nThe jenkins version that will be installed is: $jenkins_version"
-      echo '\n\nBuilding the Docker-jenkins image\n'
-      jenkins_version_run_cmd="--extra-vars "JENKINS_VERSION=$jenkins_version""
-      docker image build -t $image_name --build-arg master_port=$master_port --build-arg agent_port=$agent_port --build-arg VAULT_PWD=$vault_pass --build-arg jenkins_version=$jenkins_version  --build-arg jenkins_master_run_cmd="$jenkins_version_run_cmd" -f /home/vagrant/vibrato-code-test/docker-repo/jenkins/Dockerfile
-else
-      echo "\nThe jenkins version that will be installed is: latest"
-      echo '\nBuilding the latest version of Docker-jenkins image\n'
-      docker image build -t $image_name --build-arg master_port=$master_port --build-arg agent_port=$agent_port --build-arg VAULT_PWD=$vault_pass -f /home/vagrant/vibrato-code-test/docker-repo/jenkins/Dockerfile
-fi
+echo "\nThe jenkins version that will be installed is: latest"
+echo '\nBuilding the latest version of Docker-jenkins image\n'
+docker build -t $image_name --build-arg master_port=$master_port --build-arg agent_port=$agent_port --build-arg VAULT_PWD=$vault_pass -f /home/vagrant/vibrato-code-test/docker-repo/jenkins/Dockerfile .
 
 container_name=$image_name-app
 sleep 10s
