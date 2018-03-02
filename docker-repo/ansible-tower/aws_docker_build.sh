6f1d374929f1fb9332f21792ea0c4b7cc41f9c14
Samyak Rout <rout.samyak@nab.com.au>
2018-03-02 11:47:10 +1100

    Commit the successful tested ansible tower instalation on ubuntu Docker cotainer

---
+++ docker-repo/ansible-tower/aws_docker_build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo '\n\nPlease enter the known vault password: '
+read vault_pass
+
+echo '\n\nPlease enter a unique Image Name to be created: '
+read image_name
+
+echo '\n\nPlease enter the version of Ansile-Tower (AWX) you wish to install: '
+read tower_version
+
+echo "\n\nThe jenkins version that will be installed is: $tower_version"
+echo '\n\nBuilding the above version of Docker-jenkins image\n'
+docker build -t $image_name --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg tower_version=$tower_version --build-arg "no_proxy=$no_proxy" .
